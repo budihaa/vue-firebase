15f98bb93b23a629fd1b9c3658e552a3626589a1
Budi Haryono <budiharyono4@gmail.com>
2019-06-02 11:14:25 +0700

    create navbar component

---
+++ myproject/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+        <Navbar />
+    </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+    name: 'app',
+    components: {
+        Navbar
+    },
+    data() {
+        return {
+            msg: 'Hello, this is my first Vue Project! Woohoo :)',
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 30px;
+}
+
+h1 {
+    font-weight: normal;
+}
+</style>
---
+++ myproject/src/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav>
+        <h2>Menu</h2>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+    data() {
+        return {}
+    }
+}
+</script>
+
+// Scoped means only this component get the style,
+// even another component has element that same with this component
+<style scoped>
+    nav {
+        text-align: center;
+    }
+
+    nav h2 {
+        margin-top: 50px;
+    }
+
+    nav ul {
+        padding: 0;
+    }
+
+    nav li {
+        display: inline-block;
+        list-style: none;
+        margin: 10px;
+    }
+</style>
